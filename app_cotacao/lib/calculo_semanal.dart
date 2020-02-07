8feb849e156e925825e15efd0355e307170cbdfb
Thaíres Gonçalves <thaires.agoncalves@gmail.com>
2020-02-07 03:08:50 -0300

    corrige codigo

--- app_cotacao/lib/calculo_semanal.dart
+++ app_cotacao/lib/calculo_semanal.dart
@@ -53,136 +53,137 @@ class _SemanalState extends State<Semanal> {
           backgroundColor: Colors.black,
           title: Text("Cotação"),
           actions: <Widget>[
-             IconButton(
-                  icon: Icon(
-                    Icons.calendar_today,
-                  ),
-                  onPressed: () {
-                    showDatePicker(
-                        context: context,
-                        initialDate: DateTime.now(),
-                        firstDate: DateTime(2000),
-                        lastDate: DateTime.now(),
-                        builder: (BuildContext context, Widget child) {
-                          return Theme(
-                            data: ThemeData.dark(),
-                            child: child,
-                          );
-                        }).then((date) {
-                      setState(() {
-                        _dateTime = date;
-                        print(_dateTime);
-                        print(_dataFormatada);
-                      });
-                      // dataFormatada = _dateTime.toString();
-                      // print(dataFormatada);
+            IconButton(
+              icon: Icon(
+                Icons.calendar_today,
+              ),
+              onPressed: () {
+                showDatePicker(
+                    context: context,
+                    initialDate: DateTime.now(),
+                    firstDate: DateTime(2000),
+                    lastDate: DateTime.now(),
+                    builder: (BuildContext context, Widget child) {
+                      return Theme(
+                        data: ThemeData.dark(),
+                        child: child,
+                      );
+                    }).then((date) {
+                  setState(() {
+                    _dateTime = date;
+                    print(_dateTime);
+                    print(_dataFormatada);
+                  });
+                  // dataFormatada = _dateTime.toString();
+                  // print(dataFormatada);
+                });
+              },
+            ),
+          ],
+        ),
+        body: new Stack(children: <Widget>[
+          Image.asset(
+            "lib/images/market.jpg",
+            fit: BoxFit.cover,
+            height: 1000.0,
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.all(10),
+                child: TextField(
+                  textCapitalization: TextCapitalization.sentences,
+                  decoration: InputDecoration(
+                      labelText: 'Pesquise Aqui!',
+                      labelStyle: TextStyle(color: Colors.white),
+                      border: OutlineInputBorder()),
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.center,
+                  onSubmitted: (text) {
+                    setState(() {
+                      search = text;
                     });
                   },
                 ),
-              ],
-            ),
-        body: new Stack(
-          children: <Widget>[
-            Image.asset(
-              "lib/images/market.jpg",
-              fit: BoxFit.cover,
-              height: 1000.0,
-            ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.all(10),
-                  child: TextField(
-                    textCapitalization: TextCapitalization.sentences,
-                    decoration: InputDecoration(
-                        labelText: 'Pesquise Aqui!',
-                        labelStyle: TextStyle(color: Colors.white),
-                        border: OutlineInputBorder()),
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold),
-                    textAlign: TextAlign.center,
-                    onSubmitted: (text) {
-                      setState(() {
-                        search = text;
-                      });
-                    },
-                  ),
-                ),
-            Expanded(
-              child: FutureBuilder(
-                  future: _getStockPrice(),
-                  builder: (context, snapshot) {
-                    switch (snapshot.connectionState) {
-                      case ConnectionState.waiting:
-                      case ConnectionState.none:
-                        return Container(
-                          width: 200.0,
-                          height: 200.0,
-                          alignment: Alignment.center,
-                          child: CircularProgressIndicator(
-                            valueColor:
-                                AlwaysStoppedAnimation<Color>(Colors.white),
-                            strokeWidth: 5.0,
-                          ),
-                        );
-                      default:
-                        //Realiza calculos altes de colocar na UI
-                        _abertura = double.parse(
-                            snapshot.data["Weekly Time Series"]
-                                [_dataFormatada.toString()]["1. open"]);
-                        _alta = double.parse(snapshot.data["Weekly Time Series"]
-                            [_dataFormatada.toString()]["2. high"]);
-                        _baixa = double.parse(
-                            snapshot.data["Weekly Time Series"]
-                                [_dataFormatada.toString()]["3. low"]);
-                        _fechamento = double.parse(
-                            snapshot.data["Weekly Time Series"]
-                                [_dataFormatada.toString()]["4. close"]);
-                        _variacao = _getVariacao(_abertura, _fechamento);
+              ),
+              Expanded(
+                child: FutureBuilder(
+                    future: _getStockPrice(),
+                    builder: (context, snapshot) {
+                      switch (snapshot.connectionState) {
+                        case ConnectionState.waiting:
+                        case ConnectionState.none:
+                          return Container(
+                            width: 200.0,
+                            height: 200.0,
+                            alignment: Alignment.center,
+                            child: CircularProgressIndicator(
+                              valueColor:
+                                  AlwaysStoppedAnimation<Color>(Colors.white),
+                              strokeWidth: 5.0,
+                            ),
+                          );
+                        default:
+                          //Realiza calculos altes de colocar na UI
+                          _abertura = double.parse(
+                              snapshot.data["Weekly Time Series"]
+                                  [_dataFormatada.toString()]["1. open"]);
+                          _alta = double.parse(
+                              snapshot.data["Weekly Time Series"]
+                                  [_dataFormatada.toString()]["2. high"]);
+                          _baixa = double.parse(
+                              snapshot.data["Weekly Time Series"]
+                                  [_dataFormatada.toString()]["3. low"]);
+                          _fechamento = double.parse(
+                              snapshot.data["Weekly Time Series"]
+                                  [_dataFormatada.toString()]["4. close"]);
+                          _variacao = _getVariacao(_abertura, _fechamento);
 
-                        return Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: <Widget>[
-                              Text("Ativo: " + busca(search),
-                                      style: TextStyle(
-                                          fontSize: 30,
-                                          fontWeight: FontWeight.bold,
-                                          color: Colors.white)),
-                              Text(_variacao.toStringAsPrecision(3) + "%",
-                                  style: TextStyle(
-                                      fontSize: 70,
-                                      fontWeight: FontWeight.bold,
-                                      color: _variacao > 0
-                                          ? Colors.green
-                                          : Colors.red)),
-                              Text("Abertura: R\$" + _abertura.toString(),
-                                  style: TextStyle(
-                                      fontSize: 30,
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.white)),
-                              Text("Alta: R\$" + _alta.toString(),
-                                  style: TextStyle(
-                                      fontSize: 30,
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.white)),
-                              Text("Baixa: R\$" + _baixa.toString(),
-                                  style: TextStyle(
-                                      fontSize: 30,
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.white)),
-                              Text("Fechamento: R\$" + _fechamento.toString(),
-                                  style: TextStyle(
-                                      fontSize: 30,
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.white))
-                            ]);
-                    }
-                  }),
-            ),
-          ],
-        ));
+                          return Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                Text("Ativo: " + busca(search),
+                                    style: TextStyle(
+                                        fontSize: 30,
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.white)),
+                                Text(_variacao.toStringAsPrecision(3) + "%",
+                                    style: TextStyle(
+                                        fontSize: 70,
+                                        fontWeight: FontWeight.bold,
+                                        color: _variacao > 0
+                                            ? Colors.green
+                                            : Colors.red)),
+                                Text("Abertura: R\$" + _abertura.toString(),
+                                    style: TextStyle(
+                                        fontSize: 30,
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.white)),
+                                Text("Alta: R\$" + _alta.toString(),
+                                    style: TextStyle(
+                                        fontSize: 30,
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.white)),
+                                Text("Baixa: R\$" + _baixa.toString(),
+                                    style: TextStyle(
+                                        fontSize: 30,
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.white)),
+                                Text("Fechamento: R\$" + _fechamento.toString(),
+                                    style: TextStyle(
+                                        fontSize: 30,
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.white))
+                              ]);
+                      }
+                    }),
+              ),
+            ],
+          )
+        ]));
   }
 }
